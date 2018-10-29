648e0f15e6b130e55cf478092e32eb397e719ed6
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 19:48:50 +0000

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function getFirstSelector(selector) {
 
 function nestedTarget() {
  return document.querySelector('#nested .target')
-
 }
 
 function increaseRankBy(n) {
@@ -12,7 +11,7 @@ function increaseRankBy(n) {
 
   for (let i = 0; i < array.length; i++) {
     let this_child = array[i].children
-
+    
     for (let j = 0; j < this_child.length; j++) {
       this_child[j].innerHTML = parseInt(this_child[j].innerHTML) + n
     }
